8dc23133496bb664630d20c7b14fa1ed2549417f
4ngel19 <angelac_19@outlook.com>
2024-09-04 22:55:51 -0600

    Update xml2tps.r

--- Spanish/R/xml2tps.r
+++ Spanish/R/xml2tps.r
@@ -1,93 +1,181 @@
-if (!requireNamespace("magick", quietly = TRUE)) install.packages("magick")
-if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
-if (!requireNamespace("XML", quietly = TRUE)) install.packages("XML")
-if (!requireNamespace("stringr", quietly = TRUE)) install.packages("stringr")
-if (!requireNamespace("data.table", quietly = TRUE)) install.packages("data.table")
+#' XML2TPS
+#'
+#' @param xmlfile Name of the xml file
+#' @param tpsfile Name of the tps file it will return
+#' @param fname  Part of the images name it will erase
+#' @param LM Number of landmarks you wish to work
+#' @param image_directory Directory of the images archive you will work
+#'
+#' @return A tps file with all the data specified in the xml file
+#' @export
+#'
+#'
 
-xml2tps <- function(xmlfile, tpsfile, fname = "", LM = 0, image_directory = "") {
-  library(magick)
-  library(dplyr)
-  library(XML)
-  library(stringr)
-  library(data.table)
-  
-  get_image_heights <- function(directory) {
-    image_files <- list.files(directory, full.names = TRUE)
-    image_files <- image_files[grepl("\\.(jpg|jpeg|png|gif|bmp)$", image_files, ignore.case = TRUE)]
-    image_data <- data.frame(file = character(), height = numeric(), stringsAsFactors = FALSE)
-    for (file in image_files) {
-      img <- image_read(file)
-      info <- image_info(img)
-      image_data <- image_data %>% add_row(file = basename(file), height = info$height)
-    }   
-    return(image_data)
-  }
-  if (image_directory != "") {
-    image_heights <- get_image_heights(image_directory)
-  }
-  
-  xml_data <- xmlParse(xmlfile)
-  images <- getNodeSet(xml_data, "//image")
-  csvfile <- "output.csv"
-  csv_con <- file(csvfile, open = "wt")
-  
-  header <- c("id")
-  for (i in 0:(LM - 1)) {
-    header <- c(header, paste0("X", i), paste0("Y", i))
-  }
-  writeLines(paste(header, collapse = ","), csv_con)
-  
-  img_id_counter <- 0
-  
-  for (image in images) {
-    img_file <- xmlGetAttr(image, "file")
-    img_file <- str_replace(img_file, fname, "")
-    
-    if (image_directory != "") {
-      img_height <- image_heights %>% filter(file == img_file) %>% select(height) %>% pull()
-      top <- img_height
-    }
+xml2tps<- function(xmlfile, tpsfile, LM = 0, fname = "", img_dir = "") {
 
-    boxes <- getNodeSet(image, "box")
-    
-    for (box in boxes) {
-      row <- c(img_file)
-      parts <- getNodeSet(box, "part")
-      for (part in parts) {
-        x <- xmlGetAttr(part, "x")
-        y <- as.numeric(xmlGetAttr(part, "y"))
-        y <- abs(top - y)
-        row <- c(row, x, y)
+
+  leer_altura_jpeg <- function(ruta_archivo) {
+    con <- file(ruta_archivo, "rb")
+    contenido <- readBin(con, what = raw(), n = file.info(ruta_archivo)$size)
+    close(con)
+
+    for (i in 1:(length(contenido) - 1)) {
+      if (contenido[i] == as.raw(0xFF) && contenido[i + 1] == as.raw(0xC0)) {
+        altura <- as.integer(contenido[i + 5]) * 256 + as.integer(contenido[i + 6])
+        return(altura)
       }
-      
-      writeLines(paste(row, collapse = ","), csv_con)
-      img_id_counter <- img_id_counter + 1
+    }
+
+    stop("No se encontró la altura en el archivo JPEG")
+  }
+
+
+  leer_altura_png <- function(ruta_archivo) {
+    con <- file(ruta_archivo, "rb")
+    contenido <- readBin(con, what = raw(), n = 24)
+    close(con)
+
+    altura <- as.integer(contenido[21]) * 256^3 +
+      as.integer(contenido[22]) * 256^2 +
+      as.integer(contenido[23]) * 256 +
+      as.integer(contenido[24])
+
+    return(altura)
+  }
+
+
+  leer_altura_bmp <- function(ruta_archivo) {
+    con <- file(ruta_archivo, "rb")
+    contenido <- readBin(con, what = raw(), n = 26)
+    close(con)
+
+    altura <- as.integer(contenido[23]) +
+      as.integer(contenido[24]) * 256 +
+      as.integer(contenido[25]) * 256^2 +
+      as.integer(contenido[26]) * 256^3
+
+    return(altura)
+  }
+
+
+  leer_altura_tiff <- function(ruta_archivo) {
+    con <- file(ruta_archivo, "rb")
+    contenido <- readBin(con, what = raw(), n = 500)  # Leer una cantidad suficiente de bytes
+    close(con)
+
+
+    if (!(contenido[1] == as.raw(0x49) && contenido[2] == as.raw(0x49)) &&  # Little endian
+        !(contenido[1] == as.raw(0x4D) && contenido[2] == as.raw(0x4D))) {  # Big endian
+      stop("No es un archivo TIFF válido")
+    }
+
+    .
+    endian <- ifelse(contenido[1] == as.raw(0x49), "little", "big")
+
+
+    ifd_offset <- as.integer(readBin(contenido[5:8], what = integer(), size = 4, endian = endian))
+
+
+    num_entries <- as.integer(readBin(contenido[(ifd_offset + 1):(ifd_offset + 2)], what = integer(), size = 2, endian = endian))
+
+
+    for (i in 1:num_entries) {
+      entry_offset <- ifd_offset + 2 + (i - 1) * 12
+      tag <- as.integer(readBin(contenido[(entry_offset + 1):(entry_offset + 2)], what = integer(), size = 2, endian = endian))
+
+
+      if (tag == 256) {
+        altura <- as.integer(readBin(contenido[(entry_offset + 9):(entry_offset + 12)], what = integer(), size = 4, endian = endian))
+        return(altura)
+      }
+    }
+
+    stop("No se encontró la altura en el archivo TIFF")
+  }
+
+
+  leer_altura_imagen <- function(ruta_archivo) {
+    if (grepl("\\.jpg$|\\.jpeg$", ruta_archivo, ignore.case = TRUE)) {
+      return(leer_altura_jpeg(ruta_archivo))
+    } else if (grepl("\\.png$", ruta_archivo, ignore.case = TRUE)) {
+      return(leer_altura_png(ruta_archivo))
+    } else if (grepl("\\.bmp$", ruta_archivo, ignore.case = TRUE)) {
+      return(leer_altura_bmp(ruta_archivo))
+    } else if (grepl("\\.tiff$", ruta_archivo, ignore.case = TRUE)) {
+      return(leer_altura_tiff(ruta_archivo))
+    } else {
+      stop("Formato de imagen no soportado")
     }
   }
-  
-  close(csv_con)
-  archivo <- fread(csvfile)
-  matrix <- as.matrix(archivo)
-  nombres <- matrix[, 1]
-  coordenadas <- matrix[, -1]
-  valoresX <- coordenadas[, seq(1, ncol(coordenadas), by = 2)]
-  valoresY <- coordenadas[, seq(2, ncol(coordenadas), by = 2)]
+
+
+  leer_alturas_directorio <- function(directorio) {
+    archivos_imagenes <- list.files(directorio, pattern = "\\.(jpg|jpeg|png|bmp|tiff)$", full.names = TRUE, ignore.case = TRUE)
+    alturas <- sapply(archivos_imagenes, leer_altura_imagen)
+    names(alturas) <- basename(archivos_imagenes)  # Asignar nombres a las alturas
+    return(alturas)
+  }
+
+
+  xml_content <- readLines(xmlfile)
+
+
   con <- file(tpsfile, open = "wt")
-  
-  for (i in 1:nrow(matrix)) {
-    cat(paste("LM=", LM, sep = ""), file = con)
-    for (j in 1:ncol(valoresX)) {
-      cat(paste("\n", valoresX[i, j], valoresY[i, j], sep = " "), file = con)
+
+  row <- NULL
+  img_id_counter <- 0  # Contador de ID de imágenes
+
+
+  alturas_imagenes <- list()
+  if (img_dir != "") {
+    alturas_imagenes <- leer_alturas_directorio(img_dir)
+  }
+
+  for (i in seq_along(xml_content)) {
+    if (grepl("<image", xml_content[i])) {
+      img_file <- sub(".*file=\"([^\"]+)\".*", "\\1", xml_content[i])
+
+      if (fname != "") {
+        img_file <- sub(fname, "", img_file)
+      }
+
+
+      img_height <- NA
+      if (img_file %in% names(alturas_imagenes)) {
+        img_height <- alturas_imagenes[img_file]
+      }
+
+      row <- NULL  # Reiniciar la fila para la nueva imagen
     }
-    
-    cat(paste("\nIMAGE=", nombres[i], sep = ""), file = con)
-    cat(paste("\nID=", i - 1, sep = ""), file = con)
-    
-    if (i < nrow(matrix)) {
-      cat("\n", file = con)
+
+    if (grepl("<box", xml_content[i])) {
+
+      top <- as.numeric(sub(".*top=\"([^\"]+)\".*", "\\1", xml_content[i]))
+    }
+
+    if (grepl("<part", xml_content[i])) {
+      x <- as.numeric(sub(".*x=\"([^\"]+)\".*", "\\1", xml_content[i]))
+      y <- as.numeric(sub(".*y=\"([^\"]+)\".*", "\\1", xml_content[i]))
+
+
+      if (!is.na(img_height)) {
+        y <- img_height - y
+      }
+
+      row <- c(row, x, y)
+    }
+
+    if (length(row) == 2 * LM) {
+      cat(paste("LM=", LM, sep = ""), file = con)
+      for (j in seq(1, length(row), by = 2)) {
+        cat(paste("\n", row[j], row[j + 1], sep = " "), file = con)
+      }
+      cat(paste("\nIMAGE=", img_file, sep = ""), file = con)
+      cat(paste("\nID=", img_id_counter, sep = ""), file = con)
+      cat("\n", file = con)  # Separador para la siguiente entrada
+      img_id_counter <- img_id_counter + 1
+      row <- NULL  # Reiniciar la fila para la siguiente imagen
     }
   }
-  
   close(con)
-  unlink(csvfile)
 }
